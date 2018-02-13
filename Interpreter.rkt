8bd3c9463dde5819fad2f9a9faf1e455d99af022
Brennan McFarland <bfm21@case.edu>
2018-02-13 17:06:58 -0500

    added the really really basic stuff

--- Interpreter.rkt
+++ Interpreter.rkt
@@ -1,1 +1,15 @@
-load "simpleParser.scm"
+(load "simpleParser.scm")
+
+(define interpret
+  (lambda (filename)
+    (evaluate_tree (parser filename))
+    ; TODO: call parser with filename, evaluate parse tree,
+    ; and return the right value
+    ))
+
+(define evaluate_tree
+  (lambda tree
+    tree
+    ; TODO: instead of just returning the tree, evaluate the
+    ; parse tree and return the proper value
+    ))
