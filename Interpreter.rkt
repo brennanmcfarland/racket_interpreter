055c2a36d59903245294483f41f3031ec1c37fa4
Brennan McFarland <bfm21@case.edu>
2018-02-13 23:12:27 -0500

    implemented if, M_state_stmt now removes the first element in the list which is the operator/distinguishing element for what function to call when calling it

--- Interpreter.rkt
+++ Interpreter.rkt
@@ -20,44 +20,67 @@
   (lambda (lis s)
     (eq? (car lis) s)))
 
+(define len
+  (lambda (lis)
+	(if (null? lis)
+            0
+            (+ 1 (len (cdr lis))))))
+
 ; a helper function: given a nonterminal, determine if it is a <type>
 (define type?
-  (lambda (nonterminal)
-    (eq? nonterminal 'var))) ;TODO: this is a slight shortcut from the BNF, should probably change
+  (lambda (nterm)
+    (eq? nterm 'var))) ;TODO: this is a slight shortcut from the BNF, should probably change
 ;one or the other
+
+; a helper function: given the "operative" (the first) element in the stmt's list, call a function
+; with the parameters, eg given (= x 1) call the appropriate function with (x 1)
+(define call_on_stmt
+  (lambda (function stmt state)
+    (function (cdr stmt) state)))
 
 ; racket supports short circuit evaluation, so we can write this as one conditional
 (define M_state_stmt
-  (lambda (stmt state)
+  (lambda (nterm state)
     (cond
-      ((feq? stmt 'if) (M_state_if stmt state))
-      ((feq? stmt 'while) (M_state_while stmt state))
-      ((type? (car stmt)) (M_state_declare stmt state))
-      ((feq? stmt '=) (M_state_assign stmt state))
-      ((feq? stmt 'return) (M_state_return stmt state)) ;TODO: forgot about this in the BNF!
+      ((feq? nterm 'if) (call_on_stmt M_state_if nterm state))
+      ((feq? nterm 'while) (call_on_stmt M_state_while nterm state))
+      ((type? (car nterm)) (call_on_stmt M_state_declare nterm state))
+      ((feq? nterm '=) (call_on_stmt M_state_assign nterm state))
+      ((feq? nterm 'return) (call_on_stmt M_state_return nterm state)) ;TODO: forgot about this in the BNF!
       (else error))))
 
+(define has_else?
+  (lambda (nterm)
+    (eq? (len nterm) 3)))
+
 (define M_state_if
-  (lambda (stmt state)
-    state ;TODO
-    ))
+  (lambda (nterm state)
+    (cond
+      ((eq? (M_value_condition (car nterm) state) #t) (M_state_stmt (cadr nterm) state))
+      ((has_else? nterm) (M_state_stmt (cddr nterm) state))
+      (else state))))
 
 (define M_state_while
-  (lambda (stmt state)
+  (lambda (nterm state)
     state ;TODO
     ))
 
 (define M_state_declare
-  (lambda (stmt state)
+  (lambda (nterm state)
     state ;TODO
     ))
 
 (define M_state_assign
-  (lambda (stmt state)
+  (lambda (nterm state)
     state ;TODO
     ))
 
 (define M_state_return
-  (lambda (stmt state)
+  (lambda (nterm state)
+    state ;TODO
+    ))
+
+(define M_value_condition
+  (lambda (nterm state)
     state ;TODO
     ))
